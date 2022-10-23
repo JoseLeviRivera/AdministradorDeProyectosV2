6fccb3795113093bbee2493ceac4f1388667c6cc
JoseLeviRM <joselevirivera@gmail.com>
2022-10-23 14:42:48 -0500

    Primer commit

---
+++ src/main/java/com/example/biblioteca/AdministradorDeProyectosV2Application.java
@@ -0,0 +1,13 @@
+package com.example.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorDeProyectosV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorDeProyectosV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/biblioteca/AdministradorDeProyectosV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorDeProyectosV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
